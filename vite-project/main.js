dd3b52bf84bb99014a5c79c2dc416a68c8abab2e
Silvia Kociánová <sissi.kocianova@gmail.com>
2024-09-18 00:03:14 +0200

    helpers, orbit controls

--- vite-project/main.js
+++ vite-project/main.js
@@ -2,6 +2,10 @@ import './style.css'
 
 import * as THREE from 'three';
 
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'; //allows to move around the scene --need to add to the code
+
+
+
 const scene = new THREE.Scene();
 
 
@@ -11,29 +15,50 @@ const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 });
 
-
+//positioning
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
 renderer.render( scene, camera);
 
-
+//material, geometry
 const geometry = new THREE.TorusGeometry( 10, 3, 16, 100);
-const material = new THREE.MeshBasicMaterial({color:0xFF6347, wireframe: true});
+const material = new THREE.MeshStandardMaterial({color: 0xFF6347});
 const torus = new THREE.Mesh(geometry, material);
 
 
+
+//torus
 scene.add(torus)
 
+
+//objects
+const pointLight = new THREE.PointLight(0xffffff); //0 for a hexidecimal liberal
+pointLight.position.set(10,10,10) //the higher the number, the further the light goes from the object.
+
+const ambientLight = new THREE.AmbientLight(0xffffff); //lights up entire scene
+scene.add(pointLight, ambientLight) //i gotta add EVERY object to scene!!
+
+const lightHelper = new THREE.PointLightHelper(pointLight) //shows a lil cam with light
+const gridHelper =  new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper)
+
+
+const controls = new OrbitControls(camera, renderer.domElement); //adds controls along w import
+
+
+
+//animation
 function animate () {
   requestAnimationFrame(animate); //tells the browser I wanna perform an animation
 
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
 
+  controls.update();
 
-
-
-  
   renderer.render(scene,camera);
 }
 
